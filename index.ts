86476c8160356420639c9d3294cfb9a01da0a5bb
rajorokit <rajorokit6654@gmail.com>
2024-05-07 12:24:57 +0500

    Adding-tahira-word-counter=project

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+// Importing inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//Display a colourfull Wellcom Message
+console.log(chalk.bold.cyanBright("\n \t\t Tahira Shoaib - Word Counter"));
+console.log("=".repeat(60));
+
+//Prompt the user to enter a Sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+
+// Triming the sentence and splitting it into words on "spaces"
+let words = answers.sentence.trim().split(" ");
+
+// Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
